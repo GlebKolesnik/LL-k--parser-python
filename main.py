4f15ac64ad97796c808c4e7d4992f1c37e688cfe
Hlib <gl.kolesnichenko@gmail.com>
2023-10-20 23:38:03 +0200

    1st

--- main.py
+++ main.py
@@ -1,185 +1,252 @@
-# Необходимые импорты
-import networkx as nx
-import matplotlib.pyplot as plt
+from graphviz import Digraph
+import os
+os.environ["PATH"] += os.pathsep + 'C:\\Program Files (x86)\\Graphviz\\bin'
 
 
-# 1. LL(1)-анализатор
+# Повторно определяем все функции
+def compute_first(grammar):
+    first_set = {key: set() for key in grammar}
 
-# Вспомогательные функции
-def is_terminal(symbol, grammar):
-    for rule in grammar:
-        if rule == symbol:
-            return False
-    return True
-
-
-def compute_first(symbol, grammar, computed_firsts={}):
-    if symbol in computed_firsts:
-        return computed_firsts[symbol]
-    if is_terminal(symbol, grammar):
-        return {symbol}
-    firsts = set()
-    for production in grammar[symbol]:
-        if production[0] == 'ε':
-            firsts.add('ε')
-        else:
-            for s in production:
-                s_first = compute_first(s, grammar, computed_firsts)
-                firsts.update(s_first - {'ε'})
-                if 'ε' not in s_first:
-                    break
+    def first(symbol):
+        if symbol not in grammar:
+            return {symbol}
+        if first_set[symbol]:
+            return first_set[symbol]
+        for production in grammar[symbol]:
+            if production == "ε":
+                first_set[symbol].add("ε")
             else:
-                firsts.add('ε')
-    computed_firsts[symbol] = firsts
-    return firsts
+                for s in production:
+                    first_set[symbol] |= (first(s) - {"ε"})
+                    if "ε" not in first(s):
+                        break
+                else:
+                    first_set[symbol].add("ε")
+        return first_set[symbol]
 
-
-def compute_follow(symbol, grammar, computed_follows={}):
-    if symbol in computed_follows:
-        return computed_follows[symbol]
-    follows = set()
-    if symbol == list(grammar.keys())[0]:  # If start symbol
-        follows.add('$')
-    for rule, productions in grammar.items():
-        for production in productions:
-            for i, s in enumerate(production):
-                if s == symbol:
-                    if i < len(production) - 1:
-                        beta = production[i + 1:]
-                        first_beta = set()
-                        for b in beta:
-                            first_b = compute_first(b, grammar, {})
-                            first_beta.update(first_b - {'ε'})
-                            if 'ε' not in first_b:
-                                break
-                        follows.update(first_beta)
-                    if i == len(production) - 1 or 'ε' in first_beta:
-                        follows.update(compute_follow(rule, grammar, computed_follows))
-    computed_follows[symbol] = follows
-    return follows
-
-
-def build_ll1_parsing_table(grammar):
-    table = {}
     for non_terminal in grammar:
-        table[non_terminal] = {}
-        for production in grammar[non_terminal]:
-            first_symbols = compute_first(production[0], grammar)
-            for symbol in first_symbols:
-                if symbol != 'ε':
-                    table[non_terminal][symbol] = production
-            if 'ε' in first_symbols:
-                follow_symbols = compute_follow(non_terminal, grammar)
-                for symbol in follow_symbols:
-                    table[non_terminal][symbol] = production
-    return table
+        first(non_terminal)
+
+    return first_set
 
 
-def ll1_parser(tokens, table, start_symbol):
-    stack = ['$', start_symbol]
-    idx = 0
+def compute_follow(grammar, first_set):
+    follow_set = {key: set() for key in grammar}
+    follow_set[next(iter(grammar))] = {"$"}
+
+    def first_of_string(string):
+        result = set()
+        for s in string:
+            result |= (first_set.get(s, {s}) - {"ε"})
+            if "ε" not in first_set.get(s, {s}):
+                break
+        else:
+            result.add("ε")
+        return result
+
+    changes_made = True
+    while changes_made:
+        changes_made = False
+        for non_terminal, productions in grammar.items():
+            current_follow = follow_set[non_terminal].copy()
+            for production in productions:
+                for i, symbol in enumerate(production):
+                    if symbol in grammar:
+                        follow_set[symbol] |= (first_of_string(production[i + 1:]) - {"ε"})
+                        if "ε" in first_of_string(production[i + 1:]):
+                            follow_set[symbol] |= follow_set[non_terminal]
+            if current_follow != follow_set[non_terminal]:
+                changes_made = True
+
+    return follow_set
+
+
+def construct_parsing_table(grammar, first_set, follow_set):
+    parsing_table = {}
+
+    def first_of_string(string):
+        result = set()
+        for s in string:
+            result |= (first_set.get(s, {s}) - {"ε"})
+            if "ε" not in first_set.get(s, {s}):
+                break
+        else:
+            result.add("ε")
+        return result
+
+    for non_terminal, productions in grammar.items():
+        for production in productions:
+            first_of_production = first_of_string(production)
+            for terminal in first_of_production:
+                if terminal != "ε":
+                    parsing_table[(non_terminal, terminal)] = production
+            if "ε" in first_of_production:
+                for terminal in follow_set[non_terminal]:
+                    parsing_table[(non_terminal, terminal)] = production
+
+    return parsing_table
+
+
+def ll1_parser(input_string, grammar, parsing_table, start_symbol):
+    stack = [start_symbol]
+    input_string += "$"
+    pointer = 0
+    output = []
+
     while stack:
         top = stack[-1]
-        if idx == len(tokens):
-            lookahead = '$'
-        else:
-            lookahead = tokens[idx]
-        if top == lookahead:
-            stack.pop()
-            idx += 1
-        else:
+        current_input = input_string[pointer]
+        if top in grammar:
             try:
-                production = table[top][lookahead]
+                production = parsing_table[(top, current_input)]
+                output.append(f"{top} -> {production}")
                 stack.pop()
-                if production[0] != 'ε':
-                    for symbol in reversed(production):
+                for symbol in reversed(production):
+                    if symbol != "ε":
                         stack.append(symbol)
             except KeyError:
-                return False
-    return idx == len(tokens) and not stack
+                return False, [f"Error: No rule found for ({top}, {current_input})"]
+        elif top == current_input:
+            stack.pop()
+            pointer += 1
+        else:
+            return False, [f"Error: Mismatch found with top of stack {top} and current input {current_input}"]
+
+    if pointer != len(input_string):
+        return False, [f"Error: Input not fully parsed. Remaining input: {input_string[pointer:]}"]
+
+    return True, output
 
 
-# 2. Вспомогательные функции
-# (Они были предоставлены выше и используются в LL(1)-анализаторе)
+# Пример использования:
+grammar_example = {
+    "S": ["aAB", "bBA"],
+    "A": ["a", "ε"],
+    "B": ["b"]
+}
+first_set_example = compute_first(grammar_example)
+follow_set_example = compute_follow(grammar_example, first_set_example)
+parsing_table_example = construct_parsing_table(grammar_example, first_set_example, follow_set_example)
+input_test = "aabb"
+success, output = ll1_parser(input_test, grammar_example, parsing_table_example, "S")
+success, output
+# Добавляем команды вывода для демонстрации результатов
 
-# 3. LL(2)-анализатор
-# Примечание: этот раздел был опущен из-за сложностей
+print("Grammar:", grammar_example)
+print("\nFIRST sets:")
+for non_terminal, first in first_set_example.items():
+    print(f"FIRST({non_terminal}) = {first}")
 
-# 4. Анализатор методом рекурсивного спуска
+print("\nFOLLOW sets:")
+for non_terminal, follow in follow_set_example.items():
+    print(f"FOLLOW({non_terminal}) = {follow}")
 
-def S(tokens, idx):
-    if idx < len(tokens) and tokens[idx] == 'a':
-        idx, ast_node = A(tokens, idx + 1)
-        return idx, {'name': 'S', 'children': [{'name': 'a'}, ast_node]}
-    elif idx < len(tokens) and tokens[idx] == 'b':
-        return idx + 1, {'name': 'S', 'children': [{'name': 'b'}]}
-    return idx, None
+print("\nParsing Table:")
+for (non_terminal, terminal), production in parsing_table_example.items():
+    print(f"Table[{non_terminal}, {terminal}] = {production}")
 
+print("\nParsing Result for input:", input_test)
+if success:
+    print("Parsing Successful!")
+    for step in output:
+        print(step)
+else:
+    print("Parsing Failed!")
+    for error in output:
+        print(error)
 
-def A(tokens, idx):
-    if idx < len(tokens) and tokens[idx] == 'a':
-        idx, ast_node = A(tokens, idx + 1)
-        return idx, {'name': 'A', 'children': [{'name': 'a'}, ast_node]}
-    elif idx < len(tokens) and tokens[idx] == 'b':
-        idx, ast_node = B(tokens, idx + 1)
-        return idx, {'name': 'A', 'children': [{'name': 'b'}, ast_node]}
-    return idx, None
+class Node:
+    def __init__(self, type, children=None, value=None):
+        self.type = type
+        self.children = children or []
+        self.value = value
+class RecursiveDescentParserASTVerbose:
+    def __init__(self, input_string):
+        self.input = input_string
+        self.pointer = 0
 
+    def peek(self, k=1):
+        return self.input[self.pointer:self.pointer + k]
 
-def B(tokens, idx):
-    if idx < len(tokens) and tokens[idx] == 'b':
-        return idx + 1, {'name': 'B', 'children': [{'name': 'b'}]}
-    return idx, None
+    def consume(self):
+        self.pointer += 1
 
+    def E(self):
+        print(f"Parsing E at {self.peek()}")
+        children = []
+        t_node = self.T()
+        if t_node:
+            children.append(t_node)
+            if self.peek() == "+":
+                print(f"Matched + at {self.peek()}")
+                self.consume()
+                e_node = self.E()
+                if e_node:
+                    children.append(Node("PLUS"))
+                    children.append(e_node)
+                    return Node("E", children)
+            return Node("E", children)
+        return None
 
-def recursive_descent(tokens):
-    idx, ast = S(tokens, 0)
-    if idx == len(tokens):
-        return True, ast
-    return False, None
+    def T(self):
+        print(f"Parsing T at {self.peek()}")
+        children = []
+        f_node = self.F()
+        if f_node:
+            children.append(f_node)
+            if self.peek() == "*":
+                print(f"Matched * at {self.peek()}")
+                self.consume()
+                t_node = self.T()
+                if t_node:
+                    children.append(Node("MULTIPLY"))
+                    children.append(t_node)
+                    return Node("T", children)
+            return Node("T", children)
+        return None
 
+    def F(self):
+        print(f"Parsing F at {self.peek()}")
+        if self.peek() == "(":
+            print(f"Matched ( at {self.peek()}")
+            self.consume()
+            e_node = self.E()
+            if e_node and self.peek() == ")":
+                print(f"Matched ) at {self.peek()}")
+                self.consume()
+                return Node("F", [e_node])
+        elif self.peek().isalpha() or self.peek().isdigit():
+            value = self.peek()
+            print(f"Matched id at {self.peek()}")
+            self.consume()
+            return Node("F", value=value)
+        return None
 
-# 5. Графическая визуализация AST
+    def parse(self):
+        root = self.E()
+        if root and self.pointer == len(self.input):
+            print("Parsing Successful!")
+            return root
+        print("Parsing Failed!")
+        return None
 
-def draw_ast(ast):
-    if not ast:
-        print("AST is empty. Nothing to visualize.")
-        return
+# Test the verbose recursive descent parser to produce AST
+parser_ast_verbose = RecursiveDescentParserASTVerbose("a+b*c")
+ast_root_verbose = parser_ast_verbose.parse()
 
-    G = nx.DiGraph()
-    node_count = [0]
+def visualize_ast(node, graph=None):
+    if graph is None:
+        graph = Digraph('AST', node_attr={'shape': 'box'})
+        graph.attr(size='10,10')
+    if node.value:
+        graph.node(str(id(node)), label=f"{node.type}\n{node.value}")
+    else:
+        graph.node(str(id(node)), label=f"{node.type}")
+    for child in node.children:
+        graph.edge(str(id(node)), str(id(child)))
+        visualize_ast(child, graph)
+    return graph
 
-    def add_nodes(node):
-        if node is None:
-            return
-
-        current_node = node_count[0]
-        G.add_node(current_node, label=node['name'])
-        if 'children' in node:
-            for child in node['children']:
-                node_count[0] += 1
-                G.add_edge(current_node, node_count[0])
-                add_nodes(child)
-
-    add_nodes(ast)
-
-    pos = nx.drawing.nx_agraph.graphviz_layout(G, prog="dot")
-    labels = {i: G.nodes[i]["label"] for i in G.nodes()}
-    nx.draw(G, pos, labels=labels, with_labels=True, node_size=2000, node_color="skyblue")
-    plt.show()
-
-
-# Тестирование
-if __name__ == "__main__":
-    tokens_ll1 = ["a", "b", "c"]
-    result_ll1 = ll1_parser(tokens_ll1, build_ll1_parsing_table({
-        'S': [['a', 'A', 'c'], ['b', 'B', 'c'], ['ε']],
-        'A': [['d'], ['ε']],
-        'B': [['e'], ['ε']]
-    }), 'S')
-    print(f"LL(1) parser result for {tokens_ll1}: {result_ll1}")
-
-    tokens_recursive = ["a", "a", "a", "b"]
-    result_recursive, ast_recursive = recursive_descent(tokens_recursive)
-    print(f"Recursive descent parser result for {tokens_recursive}: {result_recursive}")
-    draw_ast(ast_recursive)
+# Для визуализации дерева
+ast_graph = visualize_ast(ast_root_verbose)
+ast_graph.view()  # Это откроет файл .pdf с визуализацией дерева
